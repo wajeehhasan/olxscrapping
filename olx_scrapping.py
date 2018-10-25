3dcfcff3621337265307a34a0d92f0f1b3ae4948
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-25 12:16:03 -0700

    scraped data from olx and stored in text file for samplization

--- olx_scrapping.py
+++ olx_scrapping.py
@@ -1,6 +1,11 @@
-from requests import get
+import requests
 from bs4 import BeautifulSoup
+import csv
+city="karachi"
+product="samsung-s7/"
+url="https://www.olx.com.pk/{}/q-{}".format(city,product)
+data = requests.get(url)
+soup=BeautifulSoup(data.text,"html.parser")
+with open("olx_scrapping.txt","w") as file:
+	file.write(soup)
 
-
-url = "https://olx.com.pk"
-response = get(url)
