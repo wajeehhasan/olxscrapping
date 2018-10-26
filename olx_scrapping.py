dfcd5431bbcd0f82bb1ab7fc37b9a079739a4284
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-26 03:47:48 -0700

    mobiles titles scraped successfully

--- olx_scrapping.py
+++ olx_scrapping.py
@@ -3,9 +3,15 @@ from bs4 import BeautifulSoup
 import csv
 city="karachi"
 product="samsung-s7/"
-url="https://www.olx.com.pk/{}/q-{}".format(city,product)
+cat="mobile-phones"
+url="https://www.olx.com.pk/{}/{}/q-{}/".format(city,cat,product)
+# url="https://www.olx.com.pk/{}/q-{}".format(city,product)
+# url="http://quotes.toscrape.com"
 data = requests.get(url)
 soup=BeautifulSoup(data.text,"html.parser")
-with open("olx_scrapping.txt","w") as file:
-	file.write(soup)
 
+titles = soup.find_all(class_="lpv-item-info--description")
+# print(titles)
+
+for title in titles:
+	print(title.find("h3").get_text())
