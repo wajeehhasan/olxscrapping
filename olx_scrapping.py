ae58c03020b57496eb70b602aac44dce571aeee6
Syed Wajeeh <wajeeh.hasan322@gmail.com>
2018-10-26 07:36:59 -0700

    finally scrapped title and price of subject

--- olx_scrapping.py
+++ olx_scrapping.py
@@ -1,17 +1,25 @@
 import requests
 from bs4 import BeautifulSoup
 import csv
+from time import sleep
 city="karachi"
 product="samsung-s7/"
 cat="mobile-phones"
 url="https://www.olx.com.pk/{}/{}/q-{}/".format(city,cat,product)
 # url="https://www.olx.com.pk/{}/q-{}".format(city,product)
 # url="http://quotes.toscrape.com"
+sleep(10)
 data = requests.get(url)
 soup=BeautifulSoup(data.text,"html.parser")
 
 titles = soup.find_all(class_="lpv-item-info--description")
-# print(titles)
+price = soup.find_all(class_="lpv-item-info--price")
 
-for title in titles:
-	print(title.find("h3").get_text())
+with open("olx_scraped.txt","w") as file:
+	for title in titles:
+		file.write(title.find("h3").get_text())
+with open("olx_scraped.txt","a") as file:
+	file.write("Now prices")
+	for pr in price:
+		file.write(pr.get_text())
+
